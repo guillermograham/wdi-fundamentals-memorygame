711e0f2036f51c6a79ef942eb14dfef96dd0d51f
William Graham <williamgraham@Williams-MBP.home>
2017-11-14 10:47:04 +0000

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,7 +1,24 @@
-var cardOne = 'Queen';
-var cardTwo = 'Queen';
-var cardThree = 'King';
-var cardFour = 'King';
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+var checkForMatch = function() {
+	
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		console.log("You found a match!");
+	} else {
+		console.log("Sorry, try again.");
+	}
+}
+
+var flipCard = function(cardId) {
+	
+	console.log("User flipped " + cards[cardId]);
+
+	cardsInPlay.push(cards[cardId]);
+
+	checkForMatch();
+}
+
+flipCard(0);
+flipCard(2);
+
