a1451965e668e9814daeb80cafb0230682641864
William Graham <williamgraham@Williams-MBP.home>
2017-11-15 20:05:59 +0000

    created game logic

--- js/main.js
+++ js/main.js
@@ -25,26 +25,49 @@ var cards = [
 var cardsInPlay = [];
 
 var checkForMatch = function() {
+
+	
 	
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-		alert("You found a match!");
+			alert("You found a match!");
 	} else {
-		alert("Sorry, try again.");
+			alert("Sorry, try again.");
 	}
-}
-
-var flipCard = function(cardId) {
 	
-	console.log("User flipped " + cards[cardId].rank);
+};
+
+var flipCard = function() {
+
+	var cardId = this.getAttribute('data-id');
+	
+	/*console.log("User flipped " + cards[cardId].rank);
 	console.log(cards[cardId].cardImage);
-	console.log(cards[cardId].suit);
+	console.log(cards[cardId].suit);*/
+
+	this.setAttribute('src', cards[cardId].cardImage);
 
 	cardsInPlay.push(cards[cardId].rank);
 
+	if (cardsInPlay.length === 2) {
 
-	checkForMatch();
-}
+		checkForMatch();
 
-flipCard(0);
-flipCard(2);
+	} else {
+		return;
+	}
+};
+
+var createBoard = function() {
+
+	for (var i = 0; i < cards.length; i++) {
+
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', "images/back.png");
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+};
+
+createBoard();
 
